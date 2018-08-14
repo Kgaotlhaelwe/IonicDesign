bf6d355944af61f10c5093564a3912bba8def6ca
Kabelo Gaotlhaelwe <Kgaotlhaelwe@gmail.com>
2018-08-14 11:46:25 +0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {SecondPage} from '../../pages/second/second';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+  signUp(){
+    this.navCtrl.setRoot(SecondPage);
+
+  }
+
+}
